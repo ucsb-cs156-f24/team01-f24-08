6d76f78051741f295e70804bf6bd865bec3657d5
Christy Yu <christyyu@ucsb.edu>
2024-10-28 23:40:44 -0700

    added tests

--- src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
@@ -4,6 +4,7 @@ import edu.ucsb.cs156.example.repositories.UserRepository;
 import edu.ucsb.cs156.example.testconfig.TestConfig;
 import edu.ucsb.cs156.example.ControllerTestCase;
 import edu.ucsb.cs156.example.entities.Articles;
+import edu.ucsb.cs156.example.entities.Articles;
 import edu.ucsb.cs156.example.repositories.ArticlesRepository;
 
 import java.util.ArrayList;
@@ -36,111 +37,168 @@ import static org.mockito.Mockito.when;
 @Import(TestConfig.class)
 public class ArticlesControllerTests extends ControllerTestCase {
 
-    @MockBean
-    ArticlesRepository articlesRepository;
+        @MockBean
+        ArticlesRepository articlesRepository;
 
-    @MockBean
-    UserRepository userRepository;
+        @MockBean
+        UserRepository userRepository;
 
-    // Authorization tests for /api/articles/admin/all
+        // Authorization tests for /api/articles/admin/all
 
-    @Test
-    public void logged_out_users_cannot_get_all() throws Exception {
-        mockMvc.perform(get("/api/articles/all"))
-                .andExpect(status().is(403)); // logged out users can't get all
-    }
+        @Test
+        public void logged_out_users_cannot_get_all() throws Exception {
+                mockMvc.perform(get("/api/articles/all"))
+                                .andExpect(status().is(403)); // logged out users can't get all
+        }
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_users_can_get_all() throws Exception {
-        mockMvc.perform(get("/api/articles/all"))
-                .andExpect(status().is(200)); // logged
-    }
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_users_can_get_all() throws Exception {
+                mockMvc.perform(get("/api/articles/all"))
+                                .andExpect(status().is(200)); // logged
+        }
 
-    // Authorization tests for /api/articles/post
-    // (Perhaps should also have these for put and delete)
+        @Test
+        public void logged_out_users_cannot_get_by_id() throws Exception {
+                mockMvc.perform(get("/api/articles?id=7"))
+                                .andExpect(status().is(403)); // logged out users can't get by id
+        }
 
-    @Test
-    public void logged_out_users_cannot_post() throws Exception {
-        mockMvc.perform(post("/api/articles/post"))
-                .andExpect(status().is(403));
-    }
+        // Authorization tests for /api/articles/post
+        // (Perhaps should also have these for put and delete)
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_regular_users_cannot_post() throws Exception {
-        mockMvc.perform(post("/api/articles/post"))
-                .andExpect(status().is(403)); // only admins can post
-    }
+        @Test
+        public void logged_out_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/articles/post"))
+                                .andExpect(status().is(403));
+        }
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_user_can_get_all_articles() throws Exception {
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_regular_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/articles/post"))
+                                .andExpect(status().is(403)); // only admins can post
+        }
 
-        // arrange
-        LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+        // // Tests with mocks for database actions
 
-        Articles article1 = Articles.builder()
-                .title("Who is the best professor at UCSB?")
-                .url("https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/")
-                .explanation("A Reddit thread discussing the best professors at UCSB.")
-                .email("noreply@reddit.com")
-                .dateAdded(ldt1)
-                .build();
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_exists() throws Exception {
 
-        LocalDateTime ldt2 = LocalDateTime.parse("2022-03-11T00:00:00");
+                // arrange
 
-        Articles article2 = Articles.builder()
-                .title("UCSB Academic Calendar")
-                .url("https://registrar.sa.ucsb.edu/calendars/academic-calendar")
-                .explanation("The official UCSB academic calendar.")
-                .email("Registration@sa.ucsb.edu")
-                .dateAdded(ldt2)
-                .build();
+                Articles article = Articles.builder()
+                                .title("Who is the best professor at UCSB?")
+                                .url("https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/")
+                                .explanation("A Reddit thread discussing the best professors at UCSB.")
+                                .email("noreply@reddit.com")
+                                .dateAdded(LocalDateTime.parse("2022-01-03T00:00:00"))
+                                .build();
 
-        ArrayList<Articles> expectedDates = new ArrayList<>();
-        expectedDates.addAll(Arrays.asList(article1, article2));
+                when(articlesRepository.findById(eq(7L))).thenReturn(Optional.of(article)); // Check not sure why id is
+                                                                                            // 7
 
-        when(articlesRepository.findAll()).thenReturn(expectedDates);
+                // act
+                MvcResult response = mockMvc.perform(get("/api/articles?id=7"))
+                                .andExpect(status().isOk()).andReturn();
 
-        // act
-        MvcResult response = mockMvc.perform(get("/api/articles/all"))
-                .andExpect(status().isOk()).andReturn();
+                // assert
 
-        // assert
+                verify(articlesRepository, times(1)).findById(eq(7L));
+                String expectedJson = mapper.writeValueAsString(article);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
 
-        verify(articlesRepository, times(1)).findAll();
-        String expectedJson = mapper.writeValueAsString(expectedDates);
-        String responseString = response.getResponse().getContentAsString();
-        assertEquals(expectedJson, responseString);
-    }
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
 
-    @WithMockUser(roles = { "ADMIN", "USER" })
-    @Test
-    public void an_admin_user_can_post_a_new_ucsbdate() throws Exception {
-        // arrange
-        LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+                // arrange
 
-        Articles article1 = Articles.builder()
-                .title("Who is the best professor at UCSB?")
-                .url("https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/")
-                .explanation("A Reddit thread discussing the best professors at UCSB.")
-                .email("noreply@reddit.com")
-                .dateAdded(ldt1)
-                .build();
+                when(articlesRepository.findById(eq(7L))).thenReturn(Optional.empty());
 
-        when(articlesRepository.save(eq(article1))).thenReturn(article1);
+                // act
+                MvcResult response = mockMvc.perform(get("/api/articles?id=7"))
+                                .andExpect(status().isNotFound()).andReturn();
 
-        // act
-        MvcResult response = mockMvc.perform(
-                post("/api/articles/post?title=Who is the best professor at UCSB?&url=https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/&explanation=A Reddit thread discussing the best professors at UCSB.&email=noreply@reddit.com&dateAdded=2022-01-03T00:00:00")
-                        .with(csrf()))
-                .andExpect(status().isOk()).andReturn();
+                // assert
 
-        // assert
-        verify(articlesRepository, times(1)).save(article1);
-        String expectedJson = mapper.writeValueAsString(article1);
-        String responseString = response.getResponse().getContentAsString();
-        assertEquals(expectedJson, responseString);
-    }
+                verify(articlesRepository, times(1)).findById(eq(7L));
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("EntityNotFoundException", json.get("type"));
+                assertEquals("Articles with id 7 not found", json.get("message"));
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_user_can_get_all_articles() throws Exception {
+
+                // arrange
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+                Articles article1 = Articles.builder()
+                                .title("Who is the best professor at UCSB?")
+                                .url("https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/")
+                                .explanation("A Reddit thread discussing the best professors at UCSB.")
+                                .email("noreply@reddit.com")
+                                .dateAdded(ldt1)
+                                .build();
+
+                LocalDateTime ldt2 = LocalDateTime.parse("2022-03-11T00:00:00");
+
+                Articles article2 = Articles.builder()
+                                .title("UCSB Academic Calendar")
+                                .url("https://registrar.sa.ucsb.edu/calendars/academic-calendar")
+                                .explanation("The official UCSB academic calendar.")
+                                .email("Registration@sa.ucsb.edu")
+                                .dateAdded(ldt2)
+                                .build();
+
+                ArrayList<Articles> expectedDates = new ArrayList<>();
+                expectedDates.addAll(Arrays.asList(article1, article2));
+
+                when(articlesRepository.findAll()).thenReturn(expectedDates);
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/articles/all"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(articlesRepository, times(1)).findAll();
+                String expectedJson = mapper.writeValueAsString(expectedDates);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void an_admin_user_can_post_a_new_article() throws Exception {
+                // arrange
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+                Articles article1 = Articles.builder()
+                                .title("Who is the best professor at UCSB?")
+                                .url("https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/")
+                                .explanation("A Reddit thread discussing the best professors at UCSB.")
+                                .email("noreply@reddit.com")
+                                .dateAdded(ldt1)
+                                .build();
+
+                when(articlesRepository.save(eq(article1))).thenReturn(article1);
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                post("/api/articles/post?title=Who is the best professor at UCSB?&url=https://www.reddit.com/r/UCSantaBarbara/comments/1e46yoe/who_is_the_best_professor_youve_had_who_is_the/&explanation=A Reddit thread discussing the best professors at UCSB.&email=noreply@reddit.com&dateAdded=2022-01-03T00:00:00")
+                                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+                verify(articlesRepository, times(1)).save(article1);
+                String expectedJson = mapper.writeValueAsString(article1);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
 }
